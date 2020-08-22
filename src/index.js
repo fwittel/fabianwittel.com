60d9460c080a54c3d7758a72221a6d5c60fd4195
Fabian Wittel <fabianwittel@fw-MacBook.fritz.box>
2020-08-22 13:46:25 +0200

    Baseline

---
+++ src/index.js
@@ -0,0 +1,12 @@
+console.log("Nice you're here, I like coders ;-)")
+
+import './styles/main.scss'
+import 'tippy.js/dist/tippy.css';
+
+import tippy from 'tippy.js';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+import headerImage from './img/fabianwittel.jpg';
+
+document.getElementById('face').src = headerImage;
