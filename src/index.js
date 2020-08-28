077099d9d77548c51906aea7d456136851fce36b
Fabian Wittel <fabianwittel@fw-MacBook.fritz.box>
2020-08-28 17:25:55 +0200

    MVP GoLive

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import Icons from 'uikit/dist/js/uikit-icons';
 UIkit.use(Icons);
 
 import headerImage from './img/fabianwittel.jpg';
-import logoImage from './img/logo.jpg';
+import logoImage from './img/logo.png';
+import favicon from './img/favicon-32.png';
 
 document.querySelector('#face img').src = headerImage;
+document.querySelector('#favicon').href = favicon;
 [...document.querySelectorAll('[id^="logo"] img')].forEach(node => node.setAttribute("src", logoImage));
 
 let  init = () => UIkit.modal("#modal-cookies").show();
